bd881eb6cc460d139b5425cbe55e7b573a3b9193
FcS <FcS@10.0.2.15>
2017-01-17 11:31:25 +0100

    main

--- src/Projet/Main.java
+++ src/Projet/Main.java
@@ -3,18 +3,17 @@ package Projet;
 public class Main {
 
 	public static void main(String[] args) {
-
+		
 		Admin adm = new Admin();
 		Connexion con = new Connexion();
 		
 
 		
-		//Graphique JFrame = new Graphique();
+		//Graphique graph = new Graphique();
 		//FenetreEtudiantFormateur JFrame = new FenetreEtudiantFormateur();	
 		//FenetreAdmin JFrame = new FenetreAdmin();
 		//FenetreResponsable respon = new FenetreResponsable();
-
-		
+		adm.modifCompte();
 		
 	}
 }
