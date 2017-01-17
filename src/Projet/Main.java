4227e0063d17f091b156a05d3f645810e8326790
Flo <Flo@10.0.2.15>
2017-01-17 15:12:54 +0100

    le commit de la journée

--- src/Projet/Main.java
+++ src/Projet/Main.java
@@ -4,12 +4,12 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Admin adm = new Admin();
-		Connexion con = new Connexion();
-		
+	//	Admin adm = new Admin();
+		//Connexion con = new Connexion();
 
-		
-		Graphique JFrame = new Graphique();
+
+		Planning JFrame = new Planning();
+		//Graphique JFrame = new Graphique();
 		//FenetreEtudiantFormateur JFrame = new FenetreEtudiantFormateur();	
 		//FenetreAdmin JFrame = new FenetreAdmin();
 		//FenetreResponsable respon = new FenetreResponsable();
---
+++ src/Projet/Planning.java
@@ -0,0 +1,48 @@
+package Projet;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+
+import javax.swing.BoxLayout;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+
+public class Planning extends JFrame {
+
+	public Planning(){
+
+		setSize(400,300);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setResizable(true);
+		setLocationRelativeTo(null);
+
+		JPanel pan = new JPanel();
+		JPanel pan2 = new JPanel();
+		JPanel pan3 = new JPanel();
+		JPanel pan4 = new JPanel();
+		
+	
+		pan2.add(new JLabel("Lundi "));
+		pan2.add(new JLabel("Mardi "));
+		pan2.add(new JLabel("Mercredi "));
+		pan2.add(new JLabel("Jeudi "));
+		pan2.add(new JLabel("Vendredi"));
+		pan2.setLayout(new BoxLayout(pan2, BoxLayout.LINE_AXIS));
+		
+		pan3.setLayout( new GridLayout());
+		pan3.add(new JLabel("Salle"));
+		pan3.add(new JLabel("Matière"));
+		pan3.add(new JLabel("Formation"));
+		pan3.add(new JLabel("Promotion"));
+		
+		pan4.add(new JRadioButton());
+		
+		pan.add(pan2,BorderLayout.NORTH);
+		pan.add(pan3,BorderLayout.WEST);
+		pan.add(pan4,BorderLayout.EAST);
+	this.setContentPane(pan);
+		this.setVisible(true);
+	}
+}
