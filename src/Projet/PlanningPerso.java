32c2b3393b5815c0e51a9249b4d0aebc1b699b24
FcS <FcS@10.0.2.15>
2017-01-18 12:27:20 +0100

    modiffcs2

--- src/Projet/PlanningPerso.java
+++ src/Projet/PlanningPerso.java
@@ -1,7 +1,27 @@
 package Projet;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public abstract class PlanningPerso {
 	public void ckeckPlan(){
 		
+		String query = "SELECT DISTINCT dateresa AS Date, nomcompte AS Professeur, nommatiere as Matiere, nompromo AS Promo, idsallefk AS Salle "
+				+ "FROM public.reservation, public.compte, public.matiere, public.promo, public.salles "
+				+ "WHERE reservation.idcomptefk = compte.idcompte "
+				+ "AND reservation.idmatierefk = matiere.idmatiere "
+				+ "AND reservation.idpromofk = promo.idpromo "
+				+ "AND logcompte = ?;";
+		try {
+			PreparedStatement prepare = Connexion.getInstance().prepareStatement(query, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+			
+			prepare.setString(1, Graphique.loginCompte);
+			
+			prepare.execute();
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 }
