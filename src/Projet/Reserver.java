36d32db5d9a405c1b4e2e46426c55e2eda4c5022
allan <allan@allan-PC.home>
2017-01-18 23:53:47 +0100

    graphique resa

--- src/Projet/Reserver.java
+++ src/Projet/Reserver.java
@@ -1,22 +1,27 @@
 package Projet;
 
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class Reserver {
+public class Reserver extends JFrame{
 	int id ;
 
-	public boolean reserverSalle (){
+	public void reserverSalle(){
 
 		Scanner sc = new Scanner(System.in);
 
-		JPanel resasalle = new JPanel();
+		JPanel resaSalle = new JPanel();
 
 		JLabel date  =	new JLabel("Rentrez la date");
 		JLabel nomresa = new JLabel("Rentrez le nom de la réservation : ");
@@ -31,60 +36,93 @@ public class Reserver {
 		JTextField tnumpromo = new JTextField(15);
 		JTextField tnumsalle = new JTextField(15);
 		JTextField tnumformateur = new JTextField(15);
+		
+		JButton valider = new JButton("valider");
+		
+		resaSalle.add(date);
+		resaSalle.add(tdate);
+		
+		resaSalle.add(nomresa);
+		resaSalle.add(tnomresa);
+		
+		resaSalle.add(nummatiere);
+		resaSalle.add(tnummatiere);
+		
+		resaSalle.add(numpromo);
+		resaSalle.add(tnumpromo);
+		
+		resaSalle.add(numsalle);
+		resaSalle.add(tnumsalle);
+		
+		resaSalle.add(numformateur);
+		resaSalle.add(tnumformateur);
+		
+		this.setTitle("Reservation");
+		setLocationRelativeTo(null);
+		setSize(new Dimension(200, 500));
+		setContentPane(resaSalle);
 
-		boolean res = false;
-		String query = "INSERT INTO public.reservation (dateresa, idmatierefk, idcomptefk, idpromofk, idsallefk, nomresa)"
-				+ " VALUES (?,"
-				+ "(SELECT idmatiere FROM public.matiere WHERE idmatiere = ?),"
-				+ "(SELECT idcompte FROM public.compte WHERE idcompte = ?),"
-				+ "(SELECT idpromo FROM public.promo WHERE idpromo = ?),"
-				+ "(SELECT idsalle FROM public.salles WHERE idsalle = ?),"
-				+ "?)"
-				+ " RETURNING idresa;";
-		try {
-			PreparedStatement prepare = Connexion.getInstance().prepareStatement(query, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+		valider.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String query = "INSERT INTO public.reservation (dateresa, idmatierefk, idcomptefk, idpromofk, idsallefk, nomresa)"
+						+ " VALUES (?,"
+						+ "(SELECT idmatiere FROM public.matiere WHERE idmatiere = ?),"
+						+ "(SELECT idcompte FROM public.compte WHERE idcompte = ?),"
+						+ "(SELECT idpromo FROM public.promo WHERE idpromo = ?),"
+						+ "(SELECT idsalle FROM public.salles WHERE idsalle = ?),"
+						+ "?)"
+						+ " RETURNING idresa;";
+				try {
+					PreparedStatement prepare = Connexion.getInstance().prepareStatement(query, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
 
-			System.out.print("Rentrez la date : ");
-			//prepare.setString(1, sc.nextLine());
-			System.out.print("Rentrez le nom de la réservation : ");
-			//prepare.setString(6, sc.nextLine());
-			System.out.print("Rentrez le numero de la matiere : ");
-			//prepare.setInt(2, sc.nextInt());
-			System.out.print("Rentrez le numero de la promo : ");
-			//prepare.setInt(4, sc.nextInt());
-			System.out.print("Rentrez le numero du formateur : ");
-			//prepare.setInt(3, sc.nextInt());
-			System.out.print("Rentrez le numéro de la salle : ");
-			//prepare.setInt(5, sc.nextInt());
-			resasalle.setVisible(true);
+					System.out.print("Rentrez la date : ");
+					prepare.setString(1, tdate.getText());
+					System.out.print("Rentrez le nom de la réservation : ");
+					prepare.setString(6, tnomresa.getText());
+					System.out.print("Rentrez le numero de la matiere : ");
+					prepare.setInt(2, Integer.parseInt(tnummatiere.getText()));
+					System.out.print("Rentrez le numero de la promo : ");
+					prepare.setInt(4, Integer.parseInt(tnumpromo.getText()));
+					System.out.print("Rentrez le numero du formateur : ");
+					prepare.setInt(3, Integer.parseInt(tnumsalle.getText()));
+					System.out.print("Rentrez le numéro de la salle : ");
+					prepare.setInt(5, Integer.parseInt(tnumformateur.getText()));
+					
 
-			//On execute la requete
-			prepare.execute();
+					//On execute la requete
+					prepare.execute();
 
-			//Si la requete s'est bien passee, on recupere le id_user qui a ete genere
-			ResultSet result = prepare.getResultSet();
-			if(result.first())
-			{
-				id = result.getInt(1);
-				res = true;
+					//Si la requete s'est bien passee, on recupere le id_user qui a ete genere
+					ResultSet result = prepare.getResultSet();
+					if(result.first())
+					{
+						id = result.getInt(1);
+						
+					}
+				}
+				catch (SQLException d) {
+					d.printStackTrace();
+				}
+				
 			}
-		}
-		catch (SQLException e) {
-			e.printStackTrace();
-		}
-
-		return res;
+		});
+		
+		resaSalle.setVisible(true);
 	}
 
 	public void modifResa(){
 		Scanner sc = new Scanner(System.in);
 
-		JPanel nomresa = new JPanel();
-		JPanel dateresa = new JPanel();
-		JPanel nummatiere = new JPanel();
-		JPanel  numformateur = new JPanel();
-		JPanel numpromo = new JPanel();
-		JPanel  numsalle = new JPanel();
+		JPanel modifResa = new JPanel();
+		
+		JLabel nomresa = new JLabel();
+		JLabel dateresa = new JLabel();
+		JLabel nummatiere = new JLabel();
+		JLabel  numformateur = new JLabel();
+		JLabel numpromo = new JLabel();
+		JLabel  numsalle = new JLabel();
 
 		JTextField tnomresa = new JTextField();
 		JTextField tdateresa = new JTextField();
@@ -92,6 +130,9 @@ public class Reserver {
 		JTextField tnumformateur = new JTextField();
 		JTextField tnumpromo = new JTextField();
 		JTextField tnumsalle = new JTextField();
+		
+		JButton valier = new JButton("valider");
+		
 
 		String query = "UPDATE public.reservation SET dateresa = ?, idmatierefk = ?, idcomptefk = ?, idpromofk = ?, idsallefk = ? WHERE nomresa = ?;";
 		try {
@@ -122,20 +163,43 @@ public class Reserver {
 
 	public void supprRes(){
 		Scanner sc = new Scanner(System.in);
+		JPanel suppResa = new JPanel();
+		this.setTitle("Reservation");
+		setLocationRelativeTo(null);
+		setSize(new Dimension(200, 500));
+		setContentPane(suppResa);
+		
+		JLabel nomResa = new JLabel("Rentrez le nom de la réservation à supprimer : ");
+		
+		JTextField tnomResa = new JTextField(15);
+		
+		JButton valider = new JButton("valider");
+		
+		suppResa.add(nomResa);
+		suppResa.add(tnomResa);
+		suppResa.add(valider);
+		
+		valider.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String query = "DELETE FROM public.reservation WHERE nomresa = ?;";
+				try {
+					PreparedStatement prepare = Connexion.getInstance().prepareStatement(query, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
 
-		String query = "DELETE FROM public.reservation WHERE nomresa = ?;";
-		try {
-			PreparedStatement prepare = Connexion.getInstance().prepareStatement(query, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+					System.out.print("Rentrez le nom de la réservation à supprimer : ");
+					prepare.setString(1, tnomResa.getText());
 
-			System.out.print("Rentrez le nom de la réservation à supprimer : ");
-			prepare.setString(1, sc.nextLine());
+					prepare.execute();
+				}
+				catch (SQLException d) {
+					d.printStackTrace();
+				}
+			}
+		});
 
-			prepare.execute();
-		}
-		catch (SQLException e) {
-			e.printStackTrace();
-		}
 
+		suppResa.setVisible(true);
 		System.out.println("La réservation a bien été supprimée");
 	}
 
