9ab47f90a4bfaed70d9324322b2ea3555bb6cfa1
allan <allan@allan-PC.home>
2017-01-18 22:01:16 +0100

    commentary

--- src/Projet/Promo.java
+++ src/Projet/Promo.java
@@ -142,7 +142,7 @@ public class Promo extends JFrame{
 
 					prepare.execute();
 					ResultSet result = prepare.getResultSet();
-					  
+					  //on cherche avec cette boucle a afficher le nom et prenom des personne de la promo
 					if(result.first())
 					{
 					    while(result.next()){         
