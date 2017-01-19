11dcd310c69ec19fdb4e9b6f89356447f91fcd0d
allan <allan@10.0.2.15>
2017-01-19 14:20:35 +0100

    lok

--- src/Projet/PlanningPerso.java
+++ src/Projet/PlanningPerso.java
@@ -20,6 +20,15 @@ import javax.swing.JPanel;
 public class PlanningPerso extends JFrame {
 
 	String jour;
+	JPanel pan = new JPanel();
+	JPanel semaine = new JPanel();
+	JPanel pan3 = new JPanel();
+	JPanel pan4 = new JPanel();
+	JPanel pan5 = new JPanel();
+	JPanel pan6 = new JPanel();
+	JPanel pan7 = new JPanel();
+	JPanel pan8 = new JPanel();
+	
 	public PlanningPerso(){
 
 		setSize(800,600);
@@ -33,70 +42,60 @@ public class PlanningPerso extends JFrame {
 		Color bleu = new Color(0,110,212);
 		Color vert = new Color(0,150,20);
 		
-		JPanel pan = new JPanel();
-		JPanel pan2 = new JPanel();
-		pan2.setLayout(new BoxLayout(pan2,BoxLayout.LINE_AXIS));
-		JPanel pan3 = new JPanel();
+		semaine.setLayout(new BoxLayout(semaine,BoxLayout.LINE_AXIS));
 		pan3.setLayout(new BoxLayout(pan3,BoxLayout.LINE_AXIS));
-		JLabel ui = new JLabel("ui ui");
-		pan3.add(ui);
-		JPanel pan4 = new JPanel();
 		pan4.setLayout(new BoxLayout(pan4,BoxLayout.LINE_AXIS));
-		JPanel pan5 = new JPanel();
 		pan5.setLayout(new BoxLayout(pan5,BoxLayout.LINE_AXIS));
-		JPanel pan6 = new JPanel();
 		pan6.setLayout(new BoxLayout(pan6,BoxLayout.LINE_AXIS));
-		JPanel pan7 = new JPanel();
 		pan7.setLayout(new BoxLayout(pan7,BoxLayout.LINE_AXIS));
-		JPanel pan8 = new JPanel();
 		pan8.setLayout(new BoxLayout(pan8,BoxLayout.LINE_AXIS));
 		
 		
 		pan.setLayout(new BoxLayout(pan,BoxLayout.PAGE_AXIS));
-	
-		JOptionPane jop = new JOptionPane("boom");
-		jop.showMessageDialog(null,"Bienvenu numero 2","haut + bas + gauche + droite + bas + start",JOptionPane.INFORMATION_MESSAGE);
-		JOptionPane jop3 = new JOptionPane("boom");
-		jop3.showMessageDialog(null,"Votre planning du mois arrive ","Carré, Carré, rond , bas, haut",JOptionPane.INFORMATION_MESSAGE);
-		JOptionPane jop4 = new JOptionPane("boom");
-		jop4.showMessageDialog(null,"Passez une bonne journée "," on ne m'a pas éffacé",JOptionPane.INFORMATION_MESSAGE);
-		JOptionPane jop5 = new JOptionPane("boom");
-		JOptionPane.showMessageDialog(null,"","La Fiesta",JOptionPane.INFORMATION_MESSAGE,new ImageIcon("C:/Users/Flo/Downloads/patoche.jpg"));
-		
-		JMenuBar bar = new JMenuBar();
-		setJMenuBar(bar);
-		bar.setBackground(bleu);
-		JLabel sem = new JLabel(" Semaine ");
-		bar.add(sem);
-	
-		JButton but = new JButton(" Precédente");
-		bar.add(but);
-		JButton but2 = new JButton(" Suivante");
-		bar.add(but2);
-		
-		bar.setLayout(new FlowLayout());
+	/**
+//		JOptionPane jop = new JOptionPane("boom");
+//		jop.showMessageDialog(null,"Bienvenu numero 2","haut + bas + gauche + droite + bas + start",JOptionPane.INFORMATION_MESSAGE);
+//		JOptionPane jop3 = new JOptionPane("boom");
+//		jop3.showMessageDialog(null,"Votre planning du mois arrive ","Carré, Carré, rond , bas, haut",JOptionPane.INFORMATION_MESSAGE);
+//		JOptionPane jop4 = new JOptionPane("boom");
+//		jop4.showMessageDialog(null,"Passez une bonne journée "," on ne m'a pas éffacé",JOptionPane.INFORMATION_MESSAGE);
+//		JOptionPane jop5 = new JOptionPane("boom");
+//		JOptionPane.showMessageDialog(null,"","La Fiesta",JOptionPane.INFORMATION_MESSAGE,new ImageIcon("C:/Users/Flo/Downloads/patoche.jpg"));
+//		
+//		JMenuBar bar = new JMenuBar();
+//		setJMenuBar(bar);
+//		bar.setBackground(bleu);
+//		JLabel sem = new JLabel(" Semaine ");
+//		bar.add(sem);
+//	
+//		JButton but = new JButton(" Precédente");
+//		bar.add(but);
+//		JButton but2 = new JButton(" Suivante");
+//		bar.add(but2);
+//		
+//		bar.setLayout(new FlowLayout());
 
-		
+		**/
 	
-		pan2.setBorder(null);
+		semaine.setBorder(null);
 		JPanel logo = new JPanel();
 		JLabel logo1 = new JLabel(new ImageIcon("C:/Users/Flo/Downloads/umbrella.jpg"));
-		pan2.add(logo);
+		semaine.add(logo);
 		logo.setBackground(white);
 		logo.add(logo1);
-		pan2.add(logo);
+		semaine.add(logo);
 	
 		JPanel lundi = new JPanel();
 		JLabel lundi1 = new JLabel("Lundi");
 		lundi.add(lundi1);
-		pan2.add(lundi);
+		semaine.add(lundi);
 	
 		
 		JPanel mardi = new JPanel();
 		JLabel mardi1= new JLabel("Mardi");
 		mardi.setBackground(white);
 		mardi.add(mardi1);
-		pan2.add(mardi);
+		semaine.add(mardi);
 
 		
 		
@@ -104,25 +103,25 @@ public class PlanningPerso extends JFrame {
 		JLabel mercredi1= new JLabel("Mercredi");
 		mercredi.setBackground(white);
 		mercredi.add(mercredi1);
-		pan2.add(mercredi);
+		semaine.add(mercredi);
 	
 		
 		JPanel jeudi = new JPanel();
 		JLabel jeudi1= new JLabel("Jeudi");
 		jeudi.setBackground(white);
 		jeudi.add(jeudi1);
-		pan2.add(jeudi);
+		semaine.add(jeudi);
 	
 		
 		JPanel vendredi = new JPanel();
 		JLabel vendredi1= new JLabel("Vendredi");
 		vendredi.setBackground(white);
 		vendredi.add(vendredi1);
-		pan2.add(vendredi);
+		semaine.add(vendredi);
 		
 	
-		pan.add(pan2, BorderLayout.NORTH);
-		pan.add(pan8,BorderLayout.SOUTH);
+		pan.add(semaine, BorderLayout.NORTH);
+		pan.add(pan8,BorderLayout.CENTER);
 		pan8.add(pan3);
 		pan8.add(pan4);
 		pan8.add(pan5);
